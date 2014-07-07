d152c2d2153a8bef6595f352beabd72b3692239e
Roman Zeyde <roman.zeyde@gmail.com>
2014-07-07 09:13:43 +0300

    transfer 8kB.

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ set -u
 set -x
 set -e 
 
-dd if=/dev/urandom of=data.send bs=1024 count=1
+dd if=/dev/urandom of=data.send bs=1024 count=8
 python send.py
 python recv.py
 python errors.py data.* #python show.py tx.int16 rx.int16
