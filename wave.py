613440acf12effaa84e58124c589938ea6c134cc
Roman Zeyde <roman.zeyde@gmail.com>
2014-07-09 08:36:10 +0300

    better aplay/arecord command logging

--- wave.py
+++ wave.py
@@ -1,6 +1,9 @@
 import os
 import signal
 import subprocess as sp
+import logging
+
+log = logging.getLogger(__name__)
 
 from common import Fs
 
@@ -11,7 +14,7 @@ def record(fname, **kwargs):
     return launch('arecord', fname, '-q', '-f', 'S16_LE', '-c', '1', '-r', str(int(Fs)), **kwargs)
 
 def launch(*args, **kwargs):
-    print args
+    log.debug('$ %s', ' '.join(args))
     p = sp.Popen(args=args, **kwargs)
     p.stop = lambda: os.kill(p.pid, signal.SIGINT)
     return p
