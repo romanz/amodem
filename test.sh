bb336c157dce77413f42ea0bdfb6bdfde26ebb62
Roman Zeyde <roman.zeyde@gmail.com>
2014-07-11 10:39:24 +0300

    use wave.py to play and record audio at test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 set -u 
-set -x
 set -e 
 
-dd if=/dev/urandom of=data.send bs=1024 count=128
-python send.py
-python recv.py
-python errors.py data.* #python show.py tx.int16 rx.int16
+dd if=/dev/urandom of=data.send bs=1024 count=1
+./send.py
+
+./wave.py record rx.int16 &
+PID=$!
+
+./wave.py play   tx.int16
+sleep 1
+kill -INT $PID
+
+./recv.py
+./errors.py data.* 
+if [ -z $? ]; then
+	./show.py tx.int16 rx.int16
+fi
--- wave.py
+++ wave.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import os
 import signal
 import subprocess as sp
@@ -6,15 +7,38 @@ import logging
 log = logging.getLogger(__name__)
 
 from common import Fs
+Fs = int(Fs)
 
 def play(fname, **kwargs):
-    return launch('aplay', fname, '-q', '-f', 'S16_LE', '-c', '1', '-r', str(int(Fs)), **kwargs)
+    return launch('aplay', fname, '-q', '-f', 'S16_LE', '-c', '1', '-r', Fs, **kwargs)
 
 def record(fname, **kwargs):
-    return launch('arecord', fname, '-q', '-f', 'S16_LE', '-c', '1', '-r', str(int(Fs)), **kwargs)
+    return launch('arecord', fname, '-q', '-f', 'S16_LE', '-c', '1', '-r', Fs, **kwargs)
 
 def launch(*args, **kwargs):
+    args = map(str, args)
     log.debug('$ %s', ' '.join(args))
     p = sp.Popen(args=args, **kwargs)
     p.stop = lambda: os.kill(p.pid, signal.SIGINT)
     return p
+
+if __name__ == '__main__':
+    import logging
+    logging.basicConfig(level=logging.DEBUG, format='%(message)s')
+    import argparse
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers()
+    fmt = 'a raw audio file (16 bits at {:.1f} kHz)'.format(Fs / 1e3)
+    recorder = subparsers.add_parser('record', help='record ' + fmt)
+    recorder.add_argument('filename', default='-', help='path to the audio file to record (otherwise, use stdout)')
+    recorder.set_defaults(func=record)
+
+    player = subparsers.add_parser('play', help='play ' + fmt)
+    player.add_argument('filename', default='-', help='path to the audio file to play (otherwise, use stdin)')
+    player.set_defaults(func=play)
+
+    args = parser.parse_args()
+    p = args.func(args.filename)
+
+    import sys
+    sys.exit(p.wait())
