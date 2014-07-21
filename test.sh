871ec8bb464756f2bf7551533634b02b504f3756
Roman Zeyde <roman.zeyde@gmail.com>
2014-07-21 13:58:23 +0300

    test: run recording and decoding in parallel

--- test.sh
+++ test.sh
@@ -2,19 +2,22 @@
 set -u 
 set -e 
 
-dd if=/dev/urandom of=data.send bs=1024 count=1024 status=none
+dd if=/dev/urandom of=data.send bs=1024 count=64 status=none
 ./send.py tx.int16 < data.send
 
 killall -q arecord aplay || true
 
 ./wave.py record rx.int16 &
-./wave.py play   tx.int16
 sleep 1
+
+./recv.py rx.int16 > data.recv &
+./wave.py play   tx.int16 
 
 killall -q arecord aplay || true
 
-./recv.py rx.int16 > data.recv
 ./errors.py data.* 
+sha256sum data.* | ./colorhash.py
 if [ -z $? ]; then
 	./show.py tx.int16 rx.int16
 fi
+
