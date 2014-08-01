ce00e949749fbb2ac1f476fc0c7abde587334238
Roman Zeyde <roman.zeyde@gmail.com>
2014-08-01 10:41:08 +0300

    Fix ECC tests

--- test_ecc.py
+++ test_ecc.py
@@ -11,7 +11,6 @@ def test_random():
     r = random.Random(0)
     x = bytearray(r.randrange(0, 256) for i in range(16 * 1024))
     y = ecc.encode(x)
-    assert len(y) % ecc.BLOCK_SIZE == 0
     x_ = concat(ecc.decode(y))
     assert x_[:len(x)] == x
     assert all(v == 0 for v in x_[len(x):])
