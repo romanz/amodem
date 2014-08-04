52dbce072e88b728ddc945c76d3feec54287b192
Roman Zeyde <roman.zeyde@gmail.com>
2014-08-04 09:27:44 +0300

    config: allow command line modem configuration

--- amodem/config.py
+++ amodem/config.py
@@ -1,17 +1,27 @@
 import numpy as np
 
-Fs = 32e3
+## Parameters
+Fs = 32000.0 # sampling frequency [Hz]
+Nfreq = 8 # number of frequencies used
+Tsym = 0.001 # symbol duration [seconds]
+Nx = 8
+Ny = 8
+
 Ts = 1.0 / Fs
 
-frequencies = (1 + np.arange(8)) * 1e3
+frequencies = (1 + np.arange(Nfreq)) * 1e3
 carrier_index = 0
 Fc = frequencies[carrier_index]
 Tc = 1.0 / Fc
 
-symbols = np.array([complex(x, y)
-                   for x in np.linspace(-1, 1, 8)
-                   for y in np.linspace(-1, 1, 8)]) / np.sqrt(2)
+assert Nx == 2 ** round(np.log2(Nx))
+assert Ny == 2 ** round(np.log2(Ny))
 
-Tsym = 1e-3
+xs = np.linspace(-1, 1, Nx) if Nx > 1 else 0.0
+ys = np.linspace(-1, 1, Ny) if Ny > 1 else 0.0
+symbols = np.array([complex(x, y) for x in xs for y in ys])
+symbols = symbols / np.max(np.abs(symbols))
+
 Nsym = int(Tsym / Ts)
 baud = int(1/Tsym)
+
