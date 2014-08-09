9985a88310d1b985ae3c14bab48ac599b91b6112
Roman Zeyde <roman.zeyde@gmail.com>
2014-08-09 15:13:23 +0300

    fix import error

--- amodem/sampling.py
+++ amodem/sampling.py
@@ -67,7 +67,7 @@ class Sampler(object):
 
 
 def resample(src, dst, df=0.0):
-    import common
+    from . import common
     x = common.load(src)
     sampler = Sampler(x, Interpolator())
     sampler.freq += df
