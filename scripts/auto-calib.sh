71991daa90fe73311900c08a07fdf95c9cd78ab5
Roman Zeyde <roman.zeyde@gmail.com>
2014-08-10 10:02:33 +0300

    fix automatic calibration script.

--- scripts/auto-calib.sh
+++ scripts/auto-calib.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 killall -q aplay arecord
-python -m calib send &
-SENDER_PID=$!
-python -m calib recv
 
+./scripts/calib.py send &
+SENDER_PID=$!
+echo "Playing audio (PID=${SENDER_PID})..."
+
+echo "Recording audio (Stop with Ctrl+C)..."
+./scripts/calib.py recv
+
+echo "Stopping player..."
 kill -INT $SENDER_PID
