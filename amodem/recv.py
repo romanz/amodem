c08b0edf80534538863be73462c3f42857d415ac
Roman Zeyde <roman.zeyde@gmail.com>
2014-08-10 17:31:19 +0300

    constellation: show phase transition trails

--- amodem/recv.py
+++ amodem/recv.py
@@ -277,7 +277,7 @@ def main(args):
 
 def constellation(y, symbols, title):
     theta = np.linspace(0, 2*np.pi, 1000)
-    pylab.plot(y.real, y.imag, '.')
+    pylab.plot(y.real, y.imag, '.:')
     pylab.plot(np.cos(theta), np.sin(theta), ':')
     points = np.array(symbols)
     pylab.plot(points.real, points.imag, '+')
