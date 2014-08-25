5bc6a36e12f764cc70a9c97aa6e79b627b5de9d8
Roman Zeyde <roman.zeyde@gmail.com>
2014-08-25 12:13:11 +0300

    fix test_wave

--- tests/test_wave.py
+++ tests/test_wave.py
@@ -7,17 +7,21 @@ def test_launch():
     p = wave.launch(tool='true', fname='fname')
     assert p.wait() == 0
 
+def test_exit():
     p = wave.launch(tool='python', fname='-', stdin=sp.PIPE)
     s = b'import sys; sys.exit(42)'
     p.stdin.write(s)
     p.stdin.close()
     assert p.wait() == 42
 
+def test_io():
     p = wave.launch(tool='python', fname='-', stdin=sp.PIPE, stdout=sp.PIPE)
     s = b'Hello World!'
     p.stdin.write(b'print("' + s + b'")\n')
     p.stdin.close()
     assert p.stdout.read(len(s)) == s
 
+def test_kill():
+    p = wave.launch(tool='python', fname='-', stdin=sp.PIPE, stdout=sp.PIPE)
     p.kill()
     assert p.wait() == -signal.SIGKILL
