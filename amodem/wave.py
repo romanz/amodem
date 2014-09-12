f0ff9c22ff6a16588631902a7c4322d6067df0db
Roman Zeyde <roman.zeyde@gmail.com>
2014-09-12 19:16:08 +0300

    wave: use stdin/stdout if fname is None

--- amodem/wave.py
+++ amodem/wave.py
@@ -14,7 +14,8 @@ bytes_per_second = bytes_per_sample * Fs
 audio_format = 'S{}_LE'.format(bits_per_sample)  # PCM signed little endian
 
 
-def launch(tool, fname='-', **kwargs):
+def launch(tool, fname=None, **kwargs):
+    fname = fname or '-'
     args = [tool, fname, '-q', '-f', audio_format, '-c', '1', '-r', str(Fs)]
     log.debug('Running: %r', args)
     p = sp.Popen(args=args, **kwargs)
