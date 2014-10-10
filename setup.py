6bc5608bf596780d5ab279e1dce46e95df14844d
Roman Zeyde <roman.zeyde@gmail.com>
2014-10-10 11:28:39 +0300

    v1.2

--- setup.py
+++ setup.py
@@ -11,17 +11,20 @@ pwd = os.path.dirname(__file__)
 
 from setuptools.command.test import test as TestCommand
 
+
 class PyTest(TestCommand):
+
     def finalize_options(self):
         self.test_args = []
         self.test_suite = True
+
     def run_tests(self):
         import pytest
         sys.exit(pytest.main(['tests']))
 
 setup(
     name="amodem",
-    version="1.1",
+    version="1.2",
     description="Audio Modem Communication Library",
     author="Roman Zeyde",
     author_email="roman.zeyde@gmail.com",
@@ -29,7 +32,7 @@ setup(
     url="http://github.com/romanz/amodem",
     packages=['amodem'],
     tests_require=['pytest'],
-    cmdclass = {'test': PyTest},
+    cmdclass={'test': PyTest},
     install_requires=['numpy', 'bitarray', 'reedsolo'],
     platforms=['POSIX'],
     classifiers=[
