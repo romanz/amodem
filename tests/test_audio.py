da5e971d944234b4743583ab0fcb9185ceeb360f
Roman Zeyde <roman.zeyde@gmail.com>
2014-11-09 19:13:07 +0200

    test_audio: improve coverage

--- tests/test_audio.py
+++ tests/test_audio.py
@@ -4,8 +4,9 @@ import signal
 
 
 def test_launch():
-    p = audio.ALSA(tool='true', Fs=32000).launch(fname='fname')
+    p = audio.ALSA(tool='true', Fs=32000).launch()
     assert p.wait() == 0
+
 
 def test_exit():
     p = audio.ALSA(tool='python', Fs=32000).launch(fname='-', stdin=sp.PIPE)
@@ -14,6 +15,7 @@ def test_exit():
     p.stdin.close()
     assert p.wait() == 42
 
+
 def test_io():
     p = audio.ALSA(tool='python', Fs=32000)
     p = p.launch(fname='-', stdin=sp.PIPE, stdout=sp.PIPE)
@@ -21,6 +23,7 @@ def test_io():
     p.stdin.write(b'print("' + s + b'")\n')
     p.stdin.close()
     assert p.stdout.read(len(s)) == s
+
 
 def test_kill():
     p = audio.ALSA(tool='python', Fs=32000)
