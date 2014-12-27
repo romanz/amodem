0ae80e6d8bb55e4ae56bca8615d7c5f02746d3d0
Roman Zeyde <roman.zeyde@gmail.com>
2014-12-27 16:25:56 +0200

    setup.py: add support for Python 3.2

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: POSIX",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Topic :: Software Development :: Libraries :: Python Modules",
