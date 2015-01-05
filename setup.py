a30ee7f92c370ff69c92dedb18bcdf0ecf72b041
Roman Zeyde <roman.zeyde@gmail.com>
2015-01-05 14:23:28 +0200

    setup.py: add support for Python 2.6

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
         "Intended Audience :: Information Technology",
         "License :: OSI Approved :: MIT License",
         "Operating System :: POSIX",
+        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
