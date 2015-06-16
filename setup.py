46cd42c31c3e3eef4066d1446ca67f529b671f68
Roman Zeyde <roman.zeyde@gmail.com>
2015-06-16 10:44:45 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='sshagent',
+    version='0.1',
+    description='Using Trezor as hardware SSH agent',
+    author='Roman Zeyde',
+    author_email='roman.zeyde@gmail.com',
+    license='MIT',
+    url='http://github.com/romanz/trezor-agent',
+    packages=['sshagent'],
+    install_requires=['ecdsa', 'trezor'],
+    platforms=['POSIX'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Networking',
+        'Topic :: Communications',
+    ],
+    entry_points={'console_scripts': [
+        'trezor_agent = sshagent.trezor_agent:main'
+    ]},
+)
