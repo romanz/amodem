b3ccb4c7294b2804adef5a91455ff7d42d251f3f
Roman Zeyde <roman.zeyde@gmail.com>
2015-07-20 18:01:34 +0300

    __main__: move entry point

--- setup.py
+++ setup.py
@@ -24,6 +24,6 @@ setup(
         'Topic :: Communications',
     ],
     entry_points={'console_scripts': [
-        'trezor-agent = sshagent.trezor_agent:main'
+        'trezor-agent = sshagent.__main__:main'
     ]},
 )
