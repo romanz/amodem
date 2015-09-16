ca3070778942d88370d86ed0ec82387f2e2ecd7d
Pavol Rusnak <stick@gk2.sk>
2015-09-16 15:30:46 +0200

    don't ask for passphrase (always use empty one similarly to TREZOR Connect)

--- trezor_agent/trezor/_factory.py
+++ trezor_agent/trezor/_factory.py
@@ -5,12 +5,16 @@ def client():
     # pylint: disable=import-error
     from trezorlib.client import TrezorClient
     from trezorlib.transport_hid import HidTransport
+    from trezorlib.messages_pb2 import PassphraseAck
+
     devices = HidTransport.enumerate()
     if len(devices) != 1:
         msg = '{:d} Trezor devices found'.format(len(devices))
         raise IOError(msg)
-    return TrezorClient(HidTransport(devices[0]))
 
+    t = TrezorClient(HidTransport(devices[0]))
+    t.callback_PassphraseRequest = lambda msg: PassphraseAck(passphrase='')
+    return t
 
 def identity_type(**kwargs):
     # pylint: disable=import-error
