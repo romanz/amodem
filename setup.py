3a806c6d77a4119f49c75a0c5bd6d4cfe40bd167
Roman Zeyde <romanzeyde@google.com>
2016-01-05 19:52:52 +0200

    beta release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='trezor_agent',
-    version='0.5.2',
+    version='0.6',
     description='Using Trezor as hardware SSH agent',
     author='Roman Zeyde',
     author_email='roman.zeyde@gmail.com',
@@ -12,7 +12,7 @@ setup(
     install_requires=['ecdsa>=0.13', 'ed25519>=1.4', 'Cython>=0.23.4', 'trezor>=0.6.6'],
     platforms=['POSIX'],
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Intended Audience :: Information Technology',
         'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
