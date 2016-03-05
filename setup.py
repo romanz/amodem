ae20ae4a042601be6810971a8a3729742797a6e7
Roman Zeyde <romanzeyde@google.com>
2016-03-05 19:54:51 +0200

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='trezor_agent',
-    version='0.6.2',
+    version='0.6.3',
     description='Using Trezor as hardware SSH agent',
     author='Roman Zeyde',
     author_email='roman.zeyde@gmail.com',
