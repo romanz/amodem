f3b49ff55381eec3b96bd1764c6b8e3714eb6d1c
Roman Zeyde <romanzeyde@google.com>
2016-04-29 11:14:27 +0300

    gpg: use strict bash mode for demo

--- trezor_agent/gpg/test.sh
+++ trezor_agent/gpg/test.sh
@@ -1,6 +1,6 @@
 # NEVER RUN ON YOUR OWN REAL GPG KEYS!!!!! THEY WILL BE DELETED!!!!!
-set -x
-(cd ~/.gnupg && rm -r openpgp-revocs.d/ private-keys-v1.d/ pubring.kbx* trustdb.gpg /tmp/log *.gpg; killall gpg-agent)
+set -x -e -u
+(cd ~/.gnupg && rm -rf openpgp-revocs.d/ private-keys-v1.d/ pubring.kbx* trustdb.gpg /tmp/log *.gpg; killall gpg-agent)
 gpg2 --full-gen-key --expert
 gpg2 --export > romanz.pub
 NOW=`date +%s`
