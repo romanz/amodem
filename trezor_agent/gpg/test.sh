5c04d17c43de4edfa13fad55d0d7ae7f7640f8c2
Roman Zeyde <romanzeyde@google.com>
2016-04-30 13:32:20 +0300

    gpg: demo with ed25519 TREZOR-based keys

--- trezor_agent/gpg/test.sh
+++ trezor_agent/gpg/test.sh
@@ -1,13 +1,14 @@
 # NEVER RUN ON YOUR OWN REAL GPG KEYS!!!!! THEY WILL BE DELETED!!!!!
 set -x -e -u
+CURVE=ed25519
 (cd ~/.gnupg && rm -rf openpgp-revocs.d/ private-keys-v1.d/ pubring.kbx* trustdb.gpg /tmp/log *.gpg; killall gpg-agent || true)
 gpg2 --full-gen-key --expert
 gpg2 --export > romanz.pub
 NOW=`date +%s`
-trezor-gpg -t $NOW -v --subkey "romanz" -o subkey.pub
+trezor-gpg -t $NOW -v -e $CURVE --subkey "romanz" -o subkey.pub
 gpg2 -K
 gpg2 -vv --import <(cat romanz.pub subkey.pub)
 gpg2 -K
 
-trezor-gpg -t $NOW -v "romanz" EXAMPLE
+trezor-gpg -t $NOW -v -e $CURVE "romanz" EXAMPLE
 gpg2 --verify EXAMPLE.sig
