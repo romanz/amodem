04a878374fd4bb94d78b5fb232350f35ea6bfa84
Roman Zeyde <romanzeyde@google.com>
2016-05-07 20:38:19 +0300

    setup: add gpg subpackage

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author='Roman Zeyde',
     author_email='roman.zeyde@gmail.com',
     url='http://github.com/romanz/trezor-agent',
-    packages=['trezor_agent'],
+    packages=['trezor_agent', 'trezor_agent.gpg'],
     scripts=['trezor_agent/gpg/trezor-git-gpg-wrapper.sh'],
     install_requires=['ecdsa>=0.13', 'ed25519>=1.4', 'Cython>=0.23.4', 'trezor>=0.6.6', 'keepkey>=0.7.0', 'semver>=2.2'],
     platforms=['POSIX'],
