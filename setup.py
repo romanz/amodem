317b672addc9986c67c1127e77133e4be7d0a03a
Roman Zeyde <roman.zeyde@gmail.com>
2016-10-31 23:00:19 +0200

    setup: support Python 3.5

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
         'Operating System :: POSIX',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: System :: Networking',
         'Topic :: Communications',
