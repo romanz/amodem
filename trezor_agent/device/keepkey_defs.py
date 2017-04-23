12359938ade5bbd42b1768669f397523ef75091a
Roman Zeyde <roman.zeyde@gmail.com>
2017-04-23 21:27:02 +0300

    keepkey: fix transport import

--- trezor_agent/device/keepkey_defs.py
+++ trezor_agent/device/keepkey_defs.py
@@ -5,5 +5,5 @@
 from keepkeylib.client import CallException as Error
 from keepkeylib.client import KeepKeyClient as Client
 from keepkeylib.messages_pb2 import PassphraseAck
-from keepkeylib.transport_hid import HidTransport
+from keepkeylib.transport_hid import HidTransport as Transport
 from keepkeylib.types_pb2 import IdentityType
