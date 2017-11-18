1942e3999b40fe1c5c69f3645684b9508178d21f
Roman Zeyde <roman.zeyde@gmail.com>
2017-11-18 16:50:08 +0200

    ssh: fix exception type for missing device

--- libagent/ssh/__init__.py
+++ libagent/ssh/__init__.py
@@ -135,7 +135,7 @@ def handle_connection_error(func):
     def wrapper(*args, **kwargs):
         try:
             return func(*args, **kwargs)
-        except IOError as e:
+        except device.interface.NotFoundError as e:
             log.error('Connection error (try unplugging and replugging your device): %s', e)
             return 1
     return wrapper
