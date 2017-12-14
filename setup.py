cb14d1e00b65d5ebc17024e25e4d50687d4c1481
Roman Zeyde <roman.zeyde@gmail.com>
2017-12-14 20:45:42 +0200

    Bump version: 0.9.5 → 0.9.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='libagent',
-    version='0.9.5',
+    version='0.9.6',
     description='Using hardware wallets as SSH/GPG agent',
     author='Roman Zeyde',
     author_email='roman.zeyde@gmail.com',
