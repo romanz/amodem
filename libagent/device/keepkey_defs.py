7bdfa7609dee6a6e9dad13932eb56998023f5ecc
Eric Siegel (Rocket Hazmat) <409247+NTICompass@users.noreply.github.com>
2020-03-13 13:50:09 -0400

    Upgrade KeepKey for new libagent code
    Add get_public_node for KeepKey

--- libagent/device/keepkey_defs.py
+++ libagent/device/keepkey_defs.py
@@ -9,6 +9,9 @@ from keepkeylib.transport_hid import HidTransport
 from keepkeylib.transport_webusb import WebUsbTransport
 from keepkeylib.types_pb2 import IdentityType
 
+get_public_node = Client.get_public_node
+sign_identity = Client.sign_identity
+Client.state = None
 
 def find_device():
     """Returns first WebUSB or HID transport."""
