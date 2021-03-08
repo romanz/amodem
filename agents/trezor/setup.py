47c827519eb84fd063b591d9e6620ab3d1d3c522
Roman Zeyde <me@romanzey.de>
2021-03-08 15:30:33 +0200

    Bump dependency to latest trezorlib

--- agents/trezor/setup.py
+++ agents/trezor/setup.py
@@ -11,7 +11,7 @@ setup(
     scripts=['trezor_agent.py'],
     install_requires=[
         'libagent>=0.14.0',
-        'trezor[hidapi]>=0.12.0,<0.13'
+        'trezor[hidapi]>=0.13'
     ],
     platforms=['POSIX'],
     classifiers=[
