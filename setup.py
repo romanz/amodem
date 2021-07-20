883ecb433415a7f8398e903acbd1e2bb537bf822
Roman Zeyde <roman.zeyde@gmail.com>
2021-07-20 22:11:58 +0300

    Bump version: 1.15.2 → 1.15.3

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ class PyTest(TestCommand):
 
 setup(
     name='amodem',
-    version='1.15.2',
+    version='1.15.3',
     description='Audio Modem Communication Library',
     author='Roman Zeyde',
     author_email='dev@romanzey.de',
