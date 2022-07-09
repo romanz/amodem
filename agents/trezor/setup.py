807c25a9fcf4622a3eda884104d7ea111966e34d
Roman Zeyde <me@romanzey.de>
2022-07-09 19:20:46 +0300

    Bump 'trezor_agent' version: 0.11.0 → 0.12.0

--- agents/trezor/setup.py
+++ agents/trezor/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='trezor_agent',
-    version='0.11.0',
+    version='0.12.0',
     description='Using Trezor as hardware SSH/GPG agent',
     author='Roman Zeyde',
     author_email='roman.zeyde@gmail.com',
