ffeda35d4945e4b05736a55326b8bafc89d93075
Roman Zeyde <roman.zeyde@gmail.com>
2022-10-27 21:11:01 +0300

    Support Python 3.10

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: System :: Networking',
         'Topic :: Communications',
