d015118d7690396c6fa9888abc09af64ece23679
Roman Zeyde <me@romanzey.de>
2024-10-21 21:59:26 +0300

    Update supported Python versions

--- setup.py
+++ setup.py
@@ -37,6 +37,8 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: System :: Networking',
         'Topic :: Communications',
